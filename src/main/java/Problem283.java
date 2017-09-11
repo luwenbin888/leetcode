f4c34c4a18eebb02b84fc85266a422852a6fef9a
LU WENBIN <wenbin.lu@emc.com>
2017-09-11 17:11:33 +0800

    Problem283

---
+++ src/main/java/Problem283.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+/**
+ Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+ For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+ Note:
+ You must do this in-place without making a copy of the array.
+ Minimize the total number of operations.
+ */
+public class Problem283 {
+    public void moveZeroes(int[] nums) {
+        int nextIdx = 0;
+        for(int num: nums) {
+            if (num != 0) {
+                nums[nextIdx++] = num;
+            }
+        }
+
+        for (int i = nextIdx; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {0, 1, 0, 3, 12};
+        Problem283 problem283 = new Problem283();
+        problem283.moveZeroes(nums);
+
+        Arrays.stream(nums).forEach(num -> System.out.print(num + ","));
+        System.out.println();
+    }
+}
