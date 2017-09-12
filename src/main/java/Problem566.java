7c14dae3a3f279e53e9ada297b6b6739c508f48f
Lu Wenbin <luwenbin@alumni.nus.edu.sg>
2017-09-12 19:25:46 +0800

    problem412, problem566

---
+++ src/main/java/Problem566.java
@@ -0,0 +1,69 @@
+/**
+ In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+ You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+ The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+ If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+ Example 1:
+
+ Input:
+ nums =
+ [[1,2],
+ [3,4]]
+ r = 1, c = 4
+ Output:
+ [[1,2,3,4]]
+ Explanation:
+ The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+
+ Example 2:
+
+ Input:
+ nums =
+ [[1,2],
+ [3,4]]
+ r = 2, c = 4
+ Output:
+ [[1,2],
+ [3,4]]
+ Explanation:
+ There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+ */
+public class Problem566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int origRow = nums.length;
+        int origCol = nums[0].length;
+
+        if (origRow * origCol != r * c) {
+            return nums;
+        }
+
+        int idx = 0;
+        int[][] result = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int row = idx / origCol;
+                int col = idx % origCol;
+                result[i][j] = nums[row][col];
+                idx++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem566 problem566 = new Problem566();
+        int[][] nums = {{1,2},{3,4}};
+        int[][] result = problem566.matrixReshape(nums, 4, 1);
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
