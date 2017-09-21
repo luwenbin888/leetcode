5e1b4fd2609d0af8caace636fa5793bf788db1c2
Lu Wenbin <luwenbin@alumni.nus.edu.sg>
2017-09-21 18:05:48 +0800

    add comment problem66

--- src/main/java/Problem66.java
+++ src/main/java/Problem66.java
@@ -1,5 +1,9 @@
 /**
- * Created by luwenbin888 on 21/09/2017.
+ Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+ You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+ The digits are stored such that the most significant digit is at the head of the list.
  */
 public class Problem66 {
     public int[] plusOne(int[] digits) {
